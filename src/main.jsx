ecf30bf4d90602952a16aae1b0144d6b98cc89c3
Developer_Arijit <rijuchatterjee004@gmail.com>
2025-07-10 20:53:44 +0530

    error-resolved

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Layout from './layout.jsx'
+import Layout from './Layout.jsx'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Tour from './pages/Tour.jsx';
